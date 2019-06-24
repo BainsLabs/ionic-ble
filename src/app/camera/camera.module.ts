8d1a1814bf1654f47cd230cfe174f3d700770c60
Akshay Sharma <akshay.sharma@bainslabs.com>
2019-06-24 14:57:44 +0530

    initial commit

---
+++ src/app/camera/camera.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { CameraComponent } from './camera.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: CameraComponent
+      }
+    ])
+  ],
+  declarations: [CameraComponent]
+})
+export class CameraModule {}
